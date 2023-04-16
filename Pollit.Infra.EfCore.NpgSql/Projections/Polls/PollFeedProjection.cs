daede2f9713382091990ecf8a222f9588c54ac68
Florian Comte <comte.florian@gmail.com>
2023-04-16 17:22:24 +0200

    use inline select query instead of shitty function that doesnt work because cockroachdb is garbage

--- Pollit.Infra.EfCore.NpgSql/Projections/Polls/PollFeedProjection.cs
+++ Pollit.Infra.EfCore.NpgSql/Projections/Polls/PollFeedProjection.cs
@@ -17,7 +17,7 @@ public class PollFeedProjection : IPollFeedProjection
 
     public PaginationResult<GetPollFeedQueryResultItem> GetPollFeed(GetPollFeedQuery query, UserId? requestingUserId)
     {
-        var dbQuery = _context.PollFeedItems.FromSql(@$"select (""Polls.GetPollFeedOfUser""({requestingUserId?.ToString()})).*");
+        var dbQuery = _context.PollFeedItems.FromSql(BuildBaseQuery(requestingUserId));
         if (query.Author is not null)
             dbQuery = dbQuery.Where(x => x.Author == query.Author);
         if (query.CreatedBefore is not null)
@@ -35,4 +35,45 @@ public class PollFeedProjection : IPollFeedProjection
 
         return dbQuery.Paginate(query.PaginationOptions);
     }
+
+    private FormattableString BuildBaseQuery(UserId? requestingUserId) =>
+        $"""
+select
+    "Polls"."Id" as "PollId",
+    "Polls"."Title",
+    string_to_array("Polls"."Tags", ',') as "Tags",
+    "Polls"."CreatedAt",
+    "Options",
+    "TotalVotesCount",
+    "Users"."UserName" AS "Author",
+    "HasMyVote"
+from "Polls"
+ join (
+    select
+        "PollId",
+        coalesce(bool_or("HasMyVote"), false) as "HasMyVote",
+        case when coalesce(bool_or("HasMyVote"), false) 
+        then 
+            json_agg(
+                json_build_object('Id', "Id", 'Title', "Title", 'VotesCount', coalesce("VotesCount", 0), 'HasMyVote', coalesce("HasMyVote", false))
+            )
+        else
+            json_agg(
+                json_build_object('Id', "Id", 'Title', "Title", 'VotesCount', null, 'HasMyVote', coalesce("HasMyVote", false))
+            )
+        end as "Options",
+        coalesce(sum("VotesCount"), 0) as "TotalVotesCount"
+    from "Polls.Options"
+    left join (
+        select
+            "PollOptionId",
+            bool_or("VoterId" = {requestingUserId?.ToString()}) AS "HasMyVote",
+            count(*) as "VotesCount"
+        from "Polls.Options.Votes"
+        group by "PollOptionId"
+    ) POV on "Id" = POV."PollOptionId"
+    group by "PollId"
+) PO on "Polls"."Id" = PO."PollId"
+join "Users" on "AuthorId" = "Users"."Id"
+""";
 }
