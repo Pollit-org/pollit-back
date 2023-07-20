bc85d668a5a29d8b1797ea03533f9363e822c568
Florian Comte <comte.florian@gmail.com>
2023-07-20 15:24:24 +0200

    use swagger in prod mode

--- Pollit.Infra.Api/Program.cs
+++ Pollit.Infra.Api/Program.cs
@@ -79,11 +79,11 @@ services
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+// }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithExposedHeaders("*"));
 
